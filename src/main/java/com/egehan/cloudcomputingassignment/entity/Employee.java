90f37268768b4122e5d41695db79d9e8c4abed92
eghnacr <eghnacr@gmail.com>
2022-01-10 21:36:16 +0300

    employee entity

---
+++ src/main/java/com/egehan/cloudcomputingassignment/entity/Employee.java
@@ -0,0 +1,27 @@
+package com.egehan.cloudcomputingassignment.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "surname", nullable = false, length = 100)
+    private String surname;
+
+    @Column(name = "address", nullable = false, length = 500)
+    private String address;
+}
