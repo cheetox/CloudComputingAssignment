dc2fd2ff13cea4ead92abf4ee4edca82ee74672f
eghnacr <eghnacr@gmail.com>
2022-01-10 21:35:40 +0300

    first commit

---
+++ src/test/java/com/egehan/cloudcomputingassignment/CloudComputingAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egehan.cloudcomputingassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudComputingAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
